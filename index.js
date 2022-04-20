b2a2eecf8f9d82f56f4748bd1c7dcdfb0e0011b0
Saminuddin Shaikh <saminuddins2@gmail.com>
2022-04-20 15:29:35 +0530

    cors hoisting

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000
 const cors = require("cors");
 
+app.use(cors());
 dotenv.config({path: "./.env"});
 app.use(express.json());     //to send json api in body from postman test
 app.use("/images", express.static(path.join(__dirname,"/images")))
 
-app.use(cors())
 
 // mongoose.connect(process.env.MONGO_URL).then(console.log("connected to mongo")).catch(err=>console.log(err));
 
