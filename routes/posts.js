865b86093a257410e4f50fb3f9a1b92a4b5c25b9
Saminuddin Shaikh <saminuddins2@gmail.com>
2022-04-20 15:54:36 +0530

    changes in routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 const User = require("../models/User");      // require is importing
 const Post = require("../models/Post");
 
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
+
 //CREATE POST
 router.post("/", async (req, res)=>{
     const newPost = new Post(req.body);
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,14 @@ const User = require("../models/User");      // require is importing
 const Post = require("../models/Post");
 const bcrypt = require("bcrypt"); 
 
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
+
 
 //UPDATE USER PROF
 router.put("/:id", async (req, res)=>{
